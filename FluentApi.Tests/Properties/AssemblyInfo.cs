5e034c3ed92d3d2be182acb18ea4578d308b7a8d
Mads Mikkel Rasmussen <mara@aspit.dk>
2018-09-05 13:40:32 +0200

    Add project files.

---
+++ FluentApi.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FluentApi.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FluentApi.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("17bc1074-7862-4617-82db-91250ee5b058")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FluentApi.Tests/Tests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using FluentApi.EF;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FluentApi.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void GetAllProjects()
+        {
+            Model model = new Model();
+            var result = model.Projects.ToList();
+            Assert.AreNotEqual(result.Count, 0);
+        }
+
+        [TestMethod]
+        public void AddNewproject()
+        {
+            Model model = new Model();
+            Project p = new Project();
+            p.Name = "Something";
+            p.Description = "A lot of something";
+            model.Projects.Add(p);
+            int count = model.Projects.ToList().Count;
+            model.SaveChanges();
+            int newCount = model.Projects.ToList().Count;
+            Assert.AreEqual(newCount, count + 1);
+        }
+
+        
+        [TestMethod]
+        public void UpdateProject()
+        {
+            Model model = new Model();
+            Project p = model.Projects.Find(1);
+            string oldDescription = p.Description;
+            p.Description = ((new Random()).Next(0,Int32.MaxValue)).ToString();
+            model.SaveChanges();
+            var newP = model.Projects.Find(1);
+            Assert.AreNotEqual(oldDescription, newP.Description);
+        }
+
+        [TestMethod]
+        public void CreateUnaffiliatedTeam()
+        {
+            Team t = new Team();
+            t.Name = "Awesome Team";
+            Model model = new Model();
+            model.Teams.Add(t);
+            model.SaveChanges();
+        }
+
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+    }
+}
